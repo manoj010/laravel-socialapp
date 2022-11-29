f40494cccea9c7e6083e3c37ec669f803f69c75a
manoj010 <manojale100@gmail.com>
2022-11-29 22:06:56 +0545

    add post & delete post added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -15,5 +16,25 @@ class PostController extends Controller
         // $posts = Post::where('user_id', '=', auth()->user()->id)->get();
 
         return view('dashboard', compact('posts'));
+    }
+
+    public function addPost(Request $req) {
+        $req->validate([
+            'user_id' => 'required',
+            'title' => 'required',
+        ]);
+
+        Post::create([
+            'user_id' => $req->user_id,
+            'title' => $req->title,
+        ]);
+
+        return redirect() -> route('dashboard');
+    }
+
+    public function deletePost($id) {
+        $data = Post::find($id);
+        $data -> delete();
+        return redirect() -> route('dashboard')->with(['message'=> 'Successfully deleted!!']);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,19 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container py-4">
+    <div class="container px-4 py-4">
+        <span style="color: red; font-size: 16px;">What's on your mind?</span>
+        <form action="{{route('addPost')}}" method="post">
+            <div class="d-flex">
+                @csrf
+                <input type="hidden" name="user_id" value="{{auth()->user()->id}}">
+                <input type="text" name="title" class="form-control m-1 @error('title') is-invalid @enderror" placeholder="Post your thought">
+                <button type="submit" class="btn btn-primary m-1">Post</button>
+            </div>
+        </form>
+    </div>
+
+    <div class="container py-2">
         <table class="table table-striped table-dark border-success table-hover table-sm">
             <thead>
                 <tr>
@@ -22,7 +34,7 @@
                         <td scope="row">{{$post->user->name}}</td>
                         <td scope="row">
                         @if(auth()->user()->id == $post->user->id)
-                            Delete
+                            <button type="button" class="btn btn-danger btn-sm"><a class="text-white text-decoration-none" href="{{url('/delete/'.$post->id)}}">Delete</a></button>
                         @endif
                         </td>
                     </tr>

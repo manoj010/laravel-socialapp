7620bcb5fa3040aa6b0d0adcabb08e952a5c24ac
manoj010 <manojale100@gmail.com>
2022-11-29 13:04:57 +0545

    post & user fetch with user match

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,7 @@
                     <th scope="col" class="px-3">SN</th>
                     <th scope="col">Posts</th>
                     <th scope="col">Posted By</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -19,6 +20,11 @@
                         <th scope="row" class="px-3">{{$sn++}}</th>
                         <td scope="row">{{$post->title}}</td>
                         <td scope="row">{{$post->user->name}}</td>
+                        <td scope="row">
+                        @if(auth()->user()->id == $post->user->id)
+                            Delete
+                        @endif
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
